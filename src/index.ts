8f12a457b3dbf2a6727d52e331b8c683d99a43cf
Ithyx <lamidey.m@gmail.com>
2021-06-30 18:26:34 -0400

    Prototype pour test connection firebase

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
+import * as dotenv from "dotenv";
 import express from "express";
+import * as admin from "firebase-admin";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+let encryptedAdminSAK = process.env.MATBAY_SERVICE_ACCOUNT_KEY;
+
+if (encryptedAdminSAK == undefined) {
+	console.log("Unable to start server: firebase admin service acount key not found in ENV: MATBAY_SERVICE_ACCOUNT_KEY");
+	encryptedAdminSAK = "";
+	stop();
+}
+
+const adminSAK = JSON.parse(Buffer.from(encryptedAdminSAK, "base64").toString());
+
+admin.initializeApp({
+	credential: admin.credential.cert(adminSAK),
+	databaseURL: "https://matbactivity.firebaseio.com"
+});
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.get("/:id", (req, res) => {
+	admin
+		.auth()
+		.verifyIdToken(req.params.id)
+		.then(decodedToken => console.log(decodedToken.uid))
+		.catch(reason => console.log(reason));
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("lol sa merch pa");
 });
 
 app.listen(port, () => {
