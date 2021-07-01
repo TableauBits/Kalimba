3035f24f38f6ea007fa6247adf0ef8d9c1f9fcef
Ithyx <lamidey.m@gmail.com>
2021-07-01 15:31:54 -0400

    l'espoir

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as dotenv from "dotenv";
 import express from "express";
 import * as admin from "firebase-admin";
 import cors from "cors";
-import * as http from "http";
 import { Server, } from "socket.io";
 
 dotenv.config();
@@ -23,31 +22,23 @@ admin.initializeApp({
 	databaseURL: "https://matbactivity.firebaseio.com"
 });
 
-const app = express();
-app.use(cors());
-const httpServer = http.createServer(app);
+const server = express()
+	.use(cors())
+	.get("/auth/:id", (req, res) => {
+		admin
+			.auth()
+			.verifyIdToken(req.params.id)
+			.then(decodedToken => console.log(decodedToken.uid))
+			.catch(reason => console.log(reason));
 
-app.get("/auth/:id", (req, res) => {
-	admin
-		.auth()
-		.verifyIdToken(req.params.id)
-		.then(decodedToken => console.log(decodedToken.uid))
-		.catch(reason => console.log(reason));
+		res.send("lol sa merch");
+	})
+	.listen(port, () => console.log(`server listening on ${port}`));
 
-	res.send("lol sa merch pa");
+const io = new Server(server);
+io.on("connection", (socket) => {
+	console.log("client connected", socket);
+	socket.on("disconnect", () => console.log("clinet disconnected"));
 });
 
-const webSocketClient = new Server(httpServer);
-webSocketClient.on("connection", (socket) => {
-	console.log("a user connected");
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-	});
-	socket.on("my message", (msg) => {
-		console.log("message: " + msg);
-	});
-});
-webSocketClient.listen(httpServer);
-httpServer.listen(port, () => {
-	console.log(`Server started, listening on port ${port}`);
-});
+setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
