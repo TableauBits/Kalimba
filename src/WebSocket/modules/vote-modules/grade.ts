8a4478ee3138efaa597a91c8cad261e35bf270b4
Ithyx <lamidey.m@gmail.com>
2021-10-09 16:47:09 -0400

    Première version de la requête edit

--- src/WebSocket/modules/vote-modules/grade.ts
+++ src/WebSocket/modules/vote-modules/grade.ts
@@ -1,9 +1,10 @@
-import { createMessage, EventType, GradeResSummaryUpdate, GradeSummary, Message } from "chelys";
+import { createMessage, EventType, extractMessageData, GradeReqEdit, GradeResSummaryUpdate, GradeSummary, GradeVote, Message } from "chelys";
 import { Client } from "../../../Types/client";
 import { VoteData } from "../../../Types/vote-data";
 import { firestore } from "../../firebase";
 import { SubModule } from "../../module";
 import { FS_CONSTITUTIONS_PATH } from "../../utility";
+import { telemetry } from "../telemetry";
 
 export class GradeVoteModule extends SubModule<VoteData> {
 	prefix = "GRADE";
@@ -15,12 +16,14 @@ export class GradeVoteModule extends SubModule<VoteData> {
 		super();
 
 		this.moduleMap.set(EventType.CST_SONG_GRADE_get_summary, this.getSummary);
+		this.moduleMap.set(EventType.CST_SONG_GRADE_edit, this.edit);
 
 		firestore.doc(`${FS_CONSTITUTIONS_PATH}/${this.data.constitution.id}/votes/summary`).onSnapshot((document) => {
 			if (!document.exists) return;
 			this.summary = document.data() as GradeSummary;
 			this.summaryListeners.forEach((listener) => {
 				listener.socket.send(createMessage<GradeResSummaryUpdate>(EventType.CST_SONG_GRADE_summary_update, { summary: this.summary }));
+				telemetry.read();
 			});
 		});
 	}
@@ -48,5 +51,13 @@ export class GradeVoteModule extends SubModule<VoteData> {
 		this.summaryListeners.add(client);
 
 		client.socket.send(createMessage<GradeResSummaryUpdate>(EventType.CST_SONG_GRADE_summary_update, { summary: this.summary }));
+		telemetry.read();
+	}
+
+	private async edit(message: Message<unknown>, client: Client): Promise<void> {
+		const vote = extractMessageData<GradeReqEdit>(message).voteData;
+
+		firestore.doc(`${FS_CONSTITUTIONS_PATH}/${this.data.constitution.id}/votes/${client.uid}`).update({ [`values.${vote.songId}`]: vote.grade });
+		telemetry.write(false);
 	}
 }
