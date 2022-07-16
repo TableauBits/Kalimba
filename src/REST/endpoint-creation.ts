c8bfe32efc4b345da74a7bf69f3da270a848d545
Ithyx <lamidey.m@gmail.com>
2022-07-16 18:19:07 -0400

    Added endpoint for fetching user data
    Co-authored-by: Hugo VDBM <hugo.vdbm@icloud.com>

--- src/REST/endpoint-creation.ts
+++ src/REST/endpoint-creation.ts
@@ -2,10 +2,12 @@ import { Express } from "express";
 
 import { dashboardController } from "./controllers/dashboard/dashboard";
 import { KEEP_ALIVE_PATH, keepAliveController } from "./controllers/keep-alive/keep-alive";
+import { USER_INFO_PATH, UserInfoController } from "./controllers/user-info/user-info";
 
 export function createEndpoints(app: Express): Express {
 	app.use("/dashboard", dashboardController);
 	app.use(`/${KEEP_ALIVE_PATH}`, keepAliveController);
+	app.use(`/${USER_INFO_PATH}`, UserInfoController);
 
 	return app;
 }
